f51503f1cd017c2984533e6d8538b73a5fd6c55a
Grimsace <acegrimreaper@gmail.com>
2026-02-19 09:49:26 -0600

    merged some of the code

--- custom_widgets.go
+++ custom_widgets.go
@@ -12,6 +12,31 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+type tappableImage struct {
+	widget.BaseWidget
+	image    *fyne.Container
+	onTapped func()
+}
+
+func newTappableImage(img *fyne.Container, tapped func()) *tappableImage {
+	ti := &tappableImage{
+		image:    img,
+		onTapped: tapped,
+	}
+	ti.ExtendBaseWidget(ti)
+	return ti
+}
+
+func (t *tappableImage) CreateRenderer() fyne.WidgetRenderer {
+	return widget.NewSimpleRenderer(t.image)
+}
+
+func (t *tappableImage) Tapped(*fyne.PointEvent) {
+	if t.onTapped != nil {
+		t.onTapped()
+	}
+}
+
 // numericInputSlider combines a slider and text entry for numeric input
 type numericInputSlider struct {
 	widget.BaseWidget
